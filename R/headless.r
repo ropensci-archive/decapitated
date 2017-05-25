eac67bd28f81b988078f1a7de5a28b250887f4e6
boB Rudis <bob@rud.is>
2017-05-25 10:54:02 -0700

    first commit

---
+++ R/headless.r
@@ -0,0 +1,29 @@
+#' Run headless
+#'
+#' @md
+#' @param port remote debugging port (defaults to `9222`)
+#' @param url visit this URL on start
+#' @export
+decapitate <- function(port=9222L, url=NULL) {
+
+  tf <- tempfile(pattern = "chrome_", fileext = ".log")
+
+  args <-  c("--headless", "--disable-gpu", sprintf("--remote-debugging-port=%s", as.integer(port)))
+  if (!is.null(url)) args <- c(args, "--dump-dom", url)
+
+
+  tmp <- system2(chrome_bin, args, wait=FALSE, stdout=tf, stderr=tf)
+
+  message(sprintf("Log file is at [%s]", tf))
+
+}
+
+#' Get the Chrome Remote ws URL
+#'
+#' @export
+ws_url <- function(port=9222L) {
+  chrome_url <- sprintf("http://localhost:%s/json", as.integer(port))
+  tmp <- jsonlite::fromJSON(chrome_url, flatten=FALSE)
+  tmp$webSocketDebuggerUrl
+}
+
